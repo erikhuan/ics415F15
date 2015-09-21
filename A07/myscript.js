145e1309eba64ffe72a1514034cc80750516ec66
erikhuan <huangerik@hotmail.com>
2015-09-20 22:30:38 -1000

    A07
    Added A07 with myscript.js and demo_form.html

---
+++ A07/myscript.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Erik Huang on 9/20/2015.
+ */
+function getClasses(elem) {
+    var x = document.getElementById(elem).className;
+}
+
+function addClass(elem, className) {
+    document.getElementById(elem).className = className;
+}
